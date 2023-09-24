83f3c1b9a097477608e66a5bd4d2725b591004da
omen_rigby <igor161b@gmail.com>
2023-09-24 11:57:41 +0400

    added try/except statement around db actions prepared to show multi-session tourney results added pairs page to menu added mp history links to rating page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import itertools
 from flask import Flask, render_template, send_from_directory, make_response, request
 from util import Dict2Class, connect, nbsp_names, hcp
 from urllib.parse import urlparse
@@ -73,13 +74,15 @@ def home():
 
 @app.route('/tournaments')
 def tournaments():
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f'select tournament_id,date,scoring,players  from tournaments order by date desc')
-    data = cursor.fetchall()
-
-    cursor.execute("""select tournament_id,"number",partnership from names where "rank"='1' or "rank" like '1−%'""")
-    winners = cursor.fetchall()
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute(f'select tournament_id,date,scoring,players  from tournaments order by date desc')
+        data = cursor.fetchall()
+        cursor.execute("""select tournament_id,"number",partnership from names where "rank"='1' or "rank" like '1−%'""")
+        winners = cursor.fetchall()
+    finally:
+        conn.close()
     tournaments_list = []
     for d in data:
         winner = [w for w in winners if w[0] == d[0]][0]
@@ -87,49 +90,76 @@ def tournaments():
             'id': d[0], 'date': d[1], 'scoring': d[2], 'players': d[3], 'winner_num': winner[1],
             'winner_names': winner[2]
         }))
-    conn.close()
     return render_template('tournaments.html', tournaments=tournaments_list)
 
 
 @app.route('/pairs')
 def pairs():
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f"""select 
-    array_to_string((select array (select trim(unnest(string_to_array(partnership, ' & '))) as a order by a)), ' & ') p, 
-    avg(percent) avg_percent, count(*) played, 
-    count(case left(rank, 2) when '1−' then 1 when '1' then 1 else null end) wins 
-    from names group by p order by wins desc, avg_percent desc""")
-    data = cursor.fetchall()
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute(f"""select 
+        array_to_string((select array (select trim(unnest(string_to_array(partnership, ' & '))) as a order by a)), ' & ') p, 
+        avg(percent) avg_percent, count(*) played, 
+        count(case left(rank, 2) when '1−' then 1 when '1' then 1 else null end) wins 
+        from names where tournament_id > 0 group by p order by wins desc, avg_percent desc""")
+        data = cursor.fetchall()
+    finally:
+        conn.close()
     return render_template('pairs.html', pairs=[Dict2Class(
         {'names': d[0], 'played': d[2], 'avg': round(d[1], 2), 'wins': d[3]}) for d in data])
 
 
 @app.route('/rating')
 def rating():
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f'select full_name,rating,rank,rank_ru,last_year  from players order by rating desc')
-    data = cursor.fetchall()
-
-    conn.close()
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute(f'select id,full_name,rating,rank,rank_ru,last_year from players order by rating desc')
+        data = cursor.fetchall()
+    finally:
+        conn.close()
     return render_template('rating.html', players=[Dict2Class(
-        {'name': d[0], 'rating': d[1], 'rank': d[2], 'rank_ru': "" if d[3] == 1.6 else d[3],
-         'last_year': d[4] or ''}) for d in data])
+        {'id': d[0], 'name': d[1], 'rating': d[2], 'rank': d[3], 'rank_ru': "" if d[4] == 1.6 else d[4],
+         'last_year': d[5] or ''}) for d in data])
+
+
+@app.route('/personal/<player_id>')
+def personal(player_id):
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute(f'select full_name from players where id={player_id}')
+        player_name = cursor.fetchone()[0].strip()
+        cursor.execute(f'''WITH results AS
+(
+    select trim(unnest(string_to_array(partnership, ' & '))) player, masterpoints, "rank", names.tournament_id, 
+    "date"  from names  left join tournaments  on names.tournament_id = tournaments.tournament_id 
+    where masterpoints > 0 and names.tournament_id > 0
+)
+select results.masterpoints, results.tournament_id, results."date", results."rank" from results 
+where player = '{player_name}' order by tournament_id DESC''')
+        data = cursor.fetchall()
+    finally:
+        conn.close()
+    return render_template('personal.html', name=player_name, results=[Dict2Class({
+        'masterpoints': d[0], 'tournament_id': d[1], "date": d[2], "rank": d[3]}) for d in data])
 
 
 @app.route('/result/<tournament_id>/ranks')
 def ranks(tournament_id):
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f'select * from tournaments where tournament_id={tournament_id}')
-    data = cursor.fetchone()
-    cursor.execute(f"select * from names where tournament_id={tournament_id} order by regexp_replace(rank, '−.*', '', 'g')::int")
-    totals = cursor.fetchall()
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute(f'select * from tournaments where tournament_id={tournament_id}')
+        data = cursor.fetchone()
+        cursor.execute(f"select * from names where tournament_id={tournament_id} order by regexp_replace(rank, '[-−−–].*', '', 'g')::int")
+        totals = cursor.fetchall()
+    finally:
+        conn.close()
     totals_dict = [Dict2Class({"rank": total[3], "number": total[1], "names": nbsp_names(total[2]), "mp": total[4],
                                "percent": total[5], "masterpoints": total[6] or '', "masterpoints_ru": total[7] or ''})
                    for total in totals]
-    conn.close()
     return render_template('rankings_template_web.html', scoring= data[4], max=data[3], tables=data[2] // 2,
                            date=data[0], boards=data[1], tournament_title=data[6], totals=totals_dict,
                            tournament_id=tournament_id)
@@ -139,15 +169,20 @@ def ranks(tournament_id):
 def scorecard(tournament_id, pair_number):
     tournament_id = int(tournament_id)
     pair_number = int(pair_number)
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f'select * from tournaments where tournament_id={tournament_id}')
-    data = cursor.fetchone()
-    cursor.execute(f'select number, partnership, rank, mps, percent from names where tournament_id={tournament_id}')
-    names = cursor.fetchall()
-    pair_results = [n for n in names if n[0] == pair_number][0]
-    cursor.execute(f'select * from protocols where tournament_id={tournament_id} and (ew={pair_number} or ns={pair_number}) order by number')
-    personals = cursor.fetchall()
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute(f"select distinct number from boards where tournament_id={tournament_id}")
+        played_boards = set(b[0] for b in cursor.fetchall())
+        cursor.execute(f'select * from tournaments where tournament_id={tournament_id}')
+        data = cursor.fetchone()
+        cursor.execute(f'select number, partnership, rank, mps, percent from names where tournament_id={tournament_id}')
+        names = cursor.fetchall()
+        pair_results = [n for n in names if n[0] == pair_number][0]
+        cursor.execute(f'select * from protocols where tournament_id={tournament_id} and (ew={pair_number} or ns={pair_number}) order by number')
+        personals = cursor.fetchall()
+    finally:
+        conn.close()
     max_mp = (data[2] // 2 - 1) * 2
     scoring_short = data[4].rstrip("s").replace("Cross-", "X")
     pair = Dict2Class({"name": nbsp_names(pair_results[1]), "number": pair_number, "scoring": data[4],
@@ -155,20 +190,34 @@ def scorecard(tournament_id, pair_number):
                        "percent_total": pair_results[4],
                        "rank": pair_results[2], "boards": []})
 
-    boards_per_round = data[1] // data[-1]
     vul = {'-': "−", "n": "NS", "e": "EW", "b": "ALL"}
     total_mps = 0
-    for i in range(1, data[1] + 1):
+    opps = []
+    for b in played_boards:
+        result = [p for p in personals if p[1] == b]
+        opps.append([result[0][2] + result[0][3] - pair_number if result else None])
+    boards_per_round_candidates = [sum(1 for _ in group) for _, group in itertools.groupby(opps)]
+    round_sums = [sum(boards_per_round_candidates[:i]) for i in range(len(boards_per_round_candidates))]
+    round_sums.extend([data[1], data[1] * 2 - round_sums[-1]])
+    for i in range(1, max(played_boards) + 1):
+        if i not in played_boards:
+            continue
+        board_index = len(pair.boards)
         p = [pers for pers in personals if pers[1] == i]
-        if not p:
+        round_index, right_margin = [(i, round_sum) for i, round_sum in enumerate(round_sums)
+                                     if round_sum > board_index][0]
+        left_margin = round_sums[round_index - 1]
+        boards_per_round = right_margin - left_margin if board_index == left_margin else 1
+        if i in played_boards and not p:
             # not played
             pair.boards.append(Dict2Class({"number": i, "vul": vul[VULNERABILITY[i % 16]],
-                                               "dir": "", "contract": "NOT&nbsp;PLAYED",
-                                               "declarer": "", "lead": "",
-                                               "score": '', "mp": 0,
-                                               "percent": 0,
-                                               "mp_per_round": 0,
-                                               "opp_names": "BYE"}))
+                                           "dir": "", "contract": "NOT&nbsp;PLAYED",
+                                           "declarer": "", "lead": "",
+                                           "score": '', "mp": 0,
+                                           "percent": 0,
+                                           "mp_per_round": 0,
+                                           "opp_names": "BYE",
+                                           "boards_per_round": boards_per_round}))
             continue
         p = p[0]
         position = "NS" if p[2] == pair_number else 'EW'
@@ -176,10 +225,10 @@ def scorecard(tournament_id, pair_number):
         opps = p[2 + (position != 'EW')]
         opp_names = nbsp_names([n for n in names if n[0] == opps][0][1])
         mp = p[index] or 0
-        current_round = (i - 1) // boards_per_round
-
+        number_diff = i - 1 - board_index
         mp_for_round = sum(res[index] for res in personals
-                           if current_round * boards_per_round < res[1] <= (current_round + 1) * boards_per_round)
+                           if left_margin < res[1] - number_diff <= right_margin)
+
 
         pair.boards.append(Dict2Class({"number": i, "vul": vul[VULNERABILITY[i % 16]],
                                        "dir": position, "contract": p[4],
@@ -187,35 +236,43 @@ def scorecard(tournament_id, pair_number):
                                        "score": p[7] if p[7] != 1 else '', "mp": mp,
                                        "percent": round(mp/max_mp, 2),
                                        "mp_per_round": round(mp_for_round, 2),
-                                       "opp_names": opp_names}))
+                                       "opp_names": opp_names,
+                                       "boards_per_round": boards_per_round}))
         total_mps += mp
     if pair.mp_total < total_mps - 0.5:  # rounding error
         pair.penalties = pair.mp_total - total_mps
-    conn.close()
     return render_template('scorecards_template_web.html', scoring_short=scoring_short, max_mp=data[3],
-                           boards_per_round=boards_per_round, pair=pair, tournament_id=tournament_id)
+                           pair=pair, tournament_id=tournament_id)
 
 
 @app.route('/result/<tournament_id>/board/<board_number>')
 def board(tournament_id, board_number):
     tournament_id = int(tournament_id)
     board_number = int(board_number)
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f'select * from tournaments where tournament_id={tournament_id}')
-    data = cursor.fetchone()
-    cursor.execute(f'select number, partnership from names where tournament_id={tournament_id}')
-    names = cursor.fetchall()
-    cursor.execute(f'select * from protocols where tournament_id={tournament_id} and number={board_number} order by mp_ns desc')
-    personals = cursor.fetchall()
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute(f"select distinct number from boards where tournament_id={tournament_id} order by number")
+        played_boards = [b[0] for b in cursor.fetchall()]
+        cursor.execute(f'select * from tournaments where tournament_id={tournament_id}')
+        data = cursor.fetchone()
+        cursor.execute(f'select number, partnership from names where tournament_id={tournament_id}')
+        names = cursor.fetchall()
+        cursor.execute(f'select * from protocols where tournament_id={tournament_id} and number={board_number} order by mp_ns desc')
+        personals = cursor.fetchall()
 
-    cursor.execute(f'select * from boards where tournament_id={tournament_id} and number={board_number}')
-    board_data = cursor.fetchone()
+        cursor.execute(f'select * from boards where tournament_id={tournament_id} and number={board_number}')
+        board_data = cursor.fetchone()
+
+    finally:
+        conn.close()
     scoring_short = data[4].rstrip("s").replace("Cross-", "X")
     vul = {'-': "−", "n": "NS", "e": "EW", "b": "ALL"}
 
-    repl_dict = {"d": hands[(board_number - 1) % 4].upper(), "b": board_number, "v": vul[VULNERABILITY[board_number % 16]],
-                 "minimax_contract": board_data[38], "minimax_outcome": board_data[39], "minimax_url": board_data[40]}
+    repl_dict = {"d": hands[(board_number - 1) % 4].upper(), "b": board_number,
+                 "v": vul[VULNERABILITY[board_number % 16]],
+                 "minimax_contract": board_data[38], "minimax_outcome": board_data[39], "minimax_url": board_data[40],
+                 "played_boards": played_boards}
     for i, h in enumerate(hands):
         for j, s in enumerate(SUITS):
             repl_dict[f"{h}{s}"] = board_data[2 + i * 4 + j].upper().replace("T", "10")
@@ -239,6 +296,5 @@ def board(tournament_id, board_number):
                      "ew_name": nbsp_names([n[1] for n in names if n[0] == p[3]][0]), "bbo_url": p[10]}
         current_board.tables.append(Dict2Class(repl_dict))
 
-    conn.close()
     return render_template('travellers_template_web.html', scoring_short=scoring_short, board=current_board,
                            tournament_id=tournament_id, maxboard=data[1])
